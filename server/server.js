425b7f4f847fd7098ee9943d2aa69e6feeb70fb7
dhananjayadav <dhananjay2451@gmail.com>
2023-10-25 17:34:47 +0530

    deploy on netlify

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const serverless = require("serverless-http");
 require('dotenv').config();
 require("./dbConfig/dbConfig");
 require("./cloudinaryConfig/cloudinaryConfig");
@@ -11,7 +12,10 @@ app.use(cors())
 app.use(express.json({ limit: '100mb' }));
 app.use("/", userRouter);
 app.use("/image", imageRouter);
+app.use("/.netlify/functions/app", userRouter);
 
 app.listen(PORT, ()=> {
     console.log(`Server running at PORT: ${PORT}`)
 })
+
+module.exports.handler = serverless(app);
